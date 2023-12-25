622048362c159d95e2cab013d0d5946c797438ce
Vivek <vivekcse.in@gmail.com>
2023-12-25 08:21:51 +0530

    calendify app template created

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex w-full">
+      <h1 className="text-center bg-red-500 text-9xl w-full">
+        Hello calendify
+      </h1>
+    </main>
+  );
+}
